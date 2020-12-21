db569cefbd29b47d4d4dfad03129718a7deb3cae
frangeldc <filiperc93@gmail.com>
2020-12-21 20:01:53 -0300

    Adding Chapter 10 exercises

---
+++ chap10/foo/Foo.java
@@ -0,0 +1,9 @@
+package chap10.foo;
+
+public class Foo {
+	static int x;
+	
+	public void go() {
+		System.out.println(x);
+	}
+}
---
+++ chap10/foo/Foo5.java
@@ -0,0 +1,10 @@
+package chap10.foo;
+
+public class Foo5 {
+	
+	static final int x = 12;
+	
+	public void go(final int x) {
+		System.out.println(x);
+	}
+}
